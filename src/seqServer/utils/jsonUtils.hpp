fab33476ab1757feba50f1a89f8c8d12e71e7097
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-07-30 12:18:09 -0400

    no more debugging

--- src/seqServer/utils/jsonUtils.hpp
+++ src/seqServer/utils/jsonUtils.hpp
@@ -45,7 +45,7 @@ cppcms::json::value jsonToCppcmsJson(const Json::Value & val);
 
 template<typename T>
 cppcms::json::value seqsToJson(const std::vector<T> & reads, const std::string & uid){
-	std::cout << "seqsToJson: start" << std::endl;
+	//std::cout << "seqsToJson: start" << std::endl;
   cppcms::json::value ret;
   auto& seqs = ret["seqs"];
   //find number of reads
@@ -58,7 +58,7 @@ cppcms::json::value seqsToJson(const std::vector<T> & reads, const std::string &
   for(const auto & pos : iter::range(reads.size())){
   	seqs[pos]= jsonToCppcmsJson(reads[pos].seqBase_.toJson());
   }
-  std::cout << "seqsToJson: stop" << std::endl;
+  //std::cout << "seqsToJson: stop" << std::endl;
   return ret;
 }
 
