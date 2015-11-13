e13cedab42ffa4d7964afdda30a5f9002102d496
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-11-13 13:36:30 -0500

    update to bibseq and bibcpp versions;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "seqServer"
-    libs = "cppitertools,bibseq:release/2.2.1,bibcpp:release/2.2.1,cppcms"
+    libs = "cppitertools,bibseq:2.2.1,bibcpp:2.2.1,cppcms"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
