2c5cb913e5f7c4aaa6c5c40060db97ec892ea212
Nicholas Hathaway <nickjhathaway@gmail.com>
2016-07-19 22:06:36 -0400

    no message

--- etc/resources/js/own/utils.js
+++ etc/resources/js/own/utils.js
@@ -20,6 +20,45 @@
 //
 //
 
+function getPromise(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
+function getJSON(url) {
+  return getPromise(url).then(JSON.parse).catch(function(err) {
+    console.log("getJSON failed for", url, err);
+    throw err;
+  });
+}
+
+
 $.fn.scrollView = function () {
     return this.each(function () {
         $('html, body').animate({
