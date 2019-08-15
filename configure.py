a479e7a3e230cb647ce52425187edf0e03fec590
Nick Hathaway <nickjhathaway@gmail.com>
2019-08-15 12:11:09 -0700

    no message

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "seqServer"
-    libs = "njhseq:release/v2.6.3,restbed:4.6"
+    libs = "njhseq:v2.6.3,restbed:4.6"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
